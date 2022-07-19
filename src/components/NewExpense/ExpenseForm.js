90525d91e73c81d49dce6e8b83f7762ead2b7898
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2022-07-19 16:36:02 +0200

    nesto

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./expenseForm.css";
+
+const ExpenseForm = () => {
+  return <form></form>;
+};
+
+export default ExpenseForm;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./newExpense.css";
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <form></form>
+    </div>
+  );
+};
+
+export default NewExpense;
