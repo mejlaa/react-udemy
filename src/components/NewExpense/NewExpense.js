ec30e910d0156d34132d73affeacd09f702b6616
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2022-07-20 11:22:48 +0200

    hh

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./newExpense.css";
+import React from 'react';
+import './newExpense.css';
+import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
   return (
     <div className="new-expense">
-      <form></form>
+      <ExpenseForm />
     </div>
   );
 };
